a462bf404df16fea8f510d0b2840f5b85a0e1346
mnihl <95353475+mnihl@users.noreply.github.com>
2024-06-15 19:37:42 +0200

    Re-setup the project to make it cleaner, dealt with user authentication

---
+++ fitnesslogger/fitnesslogger/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+
+def welcome(request):
+    return render(request, "fitnesslogger/welcome.html")
+
+def login_view(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+    return render(request, "fitnesslogger/login.html", {"form": form})
+
+def register_view(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        else:
+            form = UserCreationForm()
+    return render(request, "fitnesslogger/register.html", {"form": form})
+
+@login_required
+def userprofile(request):
+    userprofile = UserProfile.objects.get(user = request.user)
+    return render(request, "fitnesslogger/userprofile.html", {"userprofile": userprofile})
+
+@login_required
+def home(request):
+    userprofile = UserProfile.objects.get(user = request.user)
+    return render(request, "fitnesslogger/home.html", {"userprofile": userprofile})
